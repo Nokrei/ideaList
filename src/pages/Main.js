62c0d1c620310b3453edaeb314fbacbc1c877c1d
Nok <dive877@gmail.com>
2022-10-12 20:58:21 +0400

    Move reducer outside of component

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,6 +6,50 @@ import CardModal from "../components/CardModal";
 import Card from "../components/Card";
 import styles from "../styles/Main.module.css";
 
+// Reducer
+const initialState = { ideas: [] };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return { ideas: [...state.ideas, action.payload] };
+    case "delete":
+      return {
+        ...state,
+        ideas: state.ideas.filter(
+          (idea) => idea.createdAtExact !== action.payload
+        ),
+      };
+    case "enableEdit":
+      return {
+        ...state,
+        ideas: state.ideas.map((item) => {
+          if (item.createdAtExact === action.payload) {
+            const updatedItem = {
+              ...item,
+              editEnabled: true,
+            };
+            return updatedItem;
+          }
+          return item;
+        }),
+      };
+
+    case "sortByDate":
+      return {
+        ...state,
+        ideas: state.ideas.sort((a, b) =>
+          a.createdAtExact > b.createdAtExact ? 1 : -1
+        ),
+      };
+    case "sortByTitle":
+      return {
+        ...state,
+        ideas: state.ideas.sort((a, b) => (a.title > b.title ? 1 : -1)),
+      };
+  }
+}
+
 export default function MainPage() {
   // Get random ids for keys - currently using the exact date for this
   // const { v4: uuidv4 } = require("uuid");
@@ -15,49 +59,6 @@ export default function MainPage() {
   // Replaced with local storage
   const [storedIdeas, setStoredIdeas] = useLocalStorage("ideaList", []);
 
-  // Reducer
-  const initialState = { ideas: [] };
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case "add":
-        return { ideas: [...state.ideas, action.payload] };
-      case "delete":
-        return {
-          ...state,
-          ideas: state.ideas.filter(
-            (idea) => idea.createdAtExact !== action.payload
-          ),
-        };
-      case "enableEdit":
-        return {
-          ...state,
-          ideas: state.ideas.map((item) => {
-            if (item.createdAtExact === action.payload) {
-              const updatedItem = {
-                ...item,
-                editEnabled: true,
-              };
-              return updatedItem;
-            }
-            return item;
-          }),
-        };
-
-      case "sortByDate":
-        return {
-          ...state,
-          ideas: state.ideas.sort((a, b) =>
-            a.createdAtExact > b.createdAtExact ? 1 : -1
-          ),
-        };
-      case "sortByTitle":
-        return {
-          ...state,
-          ideas: state.ideas.sort((a, b) => (a.title > b.title ? 1 : -1)),
-        };
-    }
-  }
   const [state, dispatch] = useReducer(reducer, initialState);
 
   // Handle Modal open and close
